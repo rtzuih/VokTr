6cbeb4cd5e7bc2bf9100c3f9fbc7d58018e7342c
T <til.gal099@gmail.com>
2021-01-11 13:55:09 +0100

    cleaned code and fixed several bugs

--- Vok_v5.py
+++ Vok_v5.py
@@ -1,31 +1,15 @@
 import random
-from httpcore import _exceptions
-from googletrans import Translator
-from fuzzywuzzy import fuzz
 import pandas as pd
+import sys
+from fuzzywuzzy import fuzz
 from termcolor import colored
+from colorama import init
+from time import sleep
+
+
+init()
 
 # Unit = input("Aus welcher Unit möchtest du Vokabeln lernen? ")
-
-try:
-    with open("vokabeln.csv", "r") as csvfile:
-        voc_df = pd.read_csv(csvfile, sep=";")
-
-
-except FileNotFoundError:
-    print("No vocabulary data found")
-    with open("vokabeln.csv", "w") as csvfile:
-        voc_df = pd.DataFrame()
-        voc_df.to_csv(csvfile, sep=";", index=False)
-
-
-def add_vokabulary(ger_v, eng_v):
-    with open("vokabeln.csv", "w", newline="") as f:
-        voc_df.append([ger_v, eng_v], ignore_index=True)
-        if input(voc_df):
-            voc_df.to_csv(f, sep=";", index=False)
-        else:
-            print("nope")
 
 
 HELP_MSG = """
@@ -35,57 +19,74 @@ Du kannst folgende Befehle verwenden:
 "2" um dir alle Vokabeln anzeigen zu lassen
 "3" um Vokabeln hinzuzufügen
 
-!trl <Text> <Zielsprachenkürzel> (de, fr, en, etc) um etwas in eine andere Sprache zu Übersetzen
 """
 
 
 class Trainer:
-    def __init__(self, username):
+    def __init__(self):
+        try:
+            with open("vokabeln.csv", "r") as csvfile:
+                self.voc_df = pd.read_csv(csvfile, sep=";")
+
+        except:
+            print(colored("\nNo vocabulary data found or file corrupted\n",
+                          "red") + "\nCreate a new vocabulary data file?(y/n) ", end='')
+            if input() != "y":
+                print("Terminating process due to missing vocabulary data")
+                sleep(2)
+                sys.exit(sys.argv)
+            print("creating new data file\nyou can add vocabulary by typing '3'")
+            with open("vokabeln.csv", "w", newline="") as csvfile:
+                self.voc_df = pd.DataFrame(data={"Ger": ["Hallo"], "Eng": ["hello"]})
+                self.voc_df.to_csv(csvfile, sep=";", index=False)
+
+        self.username = input("Was ist dein Nutzername? ")
         try:
             with open("users.csv", "r") as csvfile:
                 self.users_df = pd.read_csv(csvfile, sep=";")
-            if username in self.users_df:
-                print(f"Welcome back {username}!\n")
+            if self.username in self.users_df:
+                print(f"Welcome back {self.username}!\n")
 
             else:
-                self.users_df[username] = [0 for i in range(len(voc_df))]
-                print(f"Welcome {username}\n")
+                self.users_df[self.username] = [0 for i in range(len(self.voc_df))]
+                print(f"Welcome {self.username}\n")
                 with open("users.csv", "w", newline="") as csvfile:
                     self.users_df.to_csv(csvfile, sep=";", index=False)
 
-        except FileNotFoundError:
+        except:
             with open("users.csv", "w", newline="") as csvfile:
-                self.users_df = pd.DataFrame(data={username: [0 for i in range(len(voc_df))]})
+                self.users_df = pd.DataFrame(data={self.username: [0 for i in range(len(self.voc_df))]})
                 self.users_df.to_csv(csvfile, sep=";", index=False)
-                print(f"New User '{username}' created")
-        except pd.errors.EmptyDataError:
+                print(f"New User '{self.username}' created")
+        if len(self.users_df[self.username]) == len(self.voc_df):
+            self.userdataanalysis()
+        else:
             with open("users.csv", "w", newline="") as csvfile:
-                self.users_df = pd.DataFrame(data={username: [0 for i in range(len(voc_df))]})
+                self.users_df[self.username] = pd.DataFrame(data={self.username: [0 for i in range(len(self.voc_df))]})
                 self.users_df.to_csv(csvfile, sep=";", index=False)
-                print(f"New User '{username}' created")
-        Trainer.userdataanalysis(self)
+                self.menu()
 
     def userdataanalysis(self):
         self.hard_to_learn = pd.DataFrame(data={"Ger": [], "Eng": []})
         for i in range(len(self.users_df)):
-            if self.users_df.loc[i, username] > 0:
-                self.hard_to_learn = self.hard_to_learn.append(voc_df.loc[i], ignore_index=True)
-                # learn_the_problematic = input(f"Willst du '{voc_df.loc[i, 'Ger']}' insbesondere üben? Du hast diese Vokabel letzes mal {self.users_df.loc[i, username]} falsch gemacht.")
+            if self.users_df.loc[i, self.username] > 0:
+                self.hard_to_learn = self.hard_to_learn.append(self.voc_df.loc[i], ignore_index=True)
+                # learn_the_problematic = input(f"Willst du '{self.voc_df.loc[i, 'Ger']}' insbesondere üben? Du hast diese Vokabel letzes mal {self.users_df.loc[i, self.username]} falsch gemacht.")
         #print("\n".join(f"{self.hard_to_learn['Ger'][i]} - {self.hard_to_learn['Eng'][i]}" for i in range(len(self.hard_to_learn))))
         if len(self.hard_to_learn) > 0:
             if input(
                     f"Letztes mal hattest du mit einigen Vokabeln besondere Schwierigkeiten. Möchtest du diese insbesondere Üben? (y/n)") == "y":
                 print("Ok, los gehts!")
                 with open("users.csv", "w", newline="") as csvfile:
-                    self.users_df[username] = pd.DataFrame(data={username: [0 for i in range(len(voc_df))]})
+                    self.users_df[self.username] = pd.DataFrame(data={self.username: [0 for i in range(len(self.voc_df))]})
                     self.users_df.to_csv(csvfile, sep=";", index=False)
-                Trainer.train(self, self.hard_to_learn)
-                Trainer.menu(self)
+                self.train(self.hard_to_learn)
+                self.menu()
 
             else:
-                Trainer.menu(self)
+                self.menu()
         else:
-            Trainer.menu(self)
+            self.menu()
 
     def menu(self):
 
@@ -95,22 +96,16 @@ class Trainer:
             if command == "exit":
                 break
             elif command == "1":
-                Trainer.train(self, voc_df)
+                self.train(self.voc_df)
 
             elif command == "2":
                 # print("\n" + "\n".join("{} - {}".format(vg, ve) for vg, ve in zip(vokabeln_g, vokabeln_e)) + "\n")
-                print("\n".join(f"{voc_df['Ger'][i]} - {voc_df['Eng'][i]}" for i in range(len(voc_df['Ger']))))
+                print("\n".join(colored(f"{self.voc_df['Ger'][i]}", "blue") +" - " + colored(f"{self.voc_df['Eng'][i]}", "magenta") for i in range(len(self.voc_df['Ger']))))
 
             elif command == "3":
-                vokg = input("Neue Deutsche Vokabel")
-                voke = input("Neue Englische Vokabel")
-                add_vokabulary(vokg, voke)
-
-            elif command[:4] == "!trl":
-                try:
-                    print(f"Übersetzung ihrer Eingabe:\n{Translator().translate(command[5:-3], command[-2:]).text}\n")
-                except _exceptions.ConnectError:
-                    print("Dein Text konnte nicht übersetzt werden.\nHast du vielleicht keine Internetverbindung?")
+                vokg = input("Neue Deutsche Vokabel ")
+                voke = input("Neue Englische Vokabel ")
+                self.add_vokabulary(vokg, voke)
 
             elif command[:4] == "help":
                 print(HELP_MSG)
@@ -124,8 +119,9 @@ class Trainer:
         mode = random.randint(0, 1)
         while len(order) != 0:
 
-            user_input = input(
-                f"\nWas ist die {'Englische' if mode == 0 else 'Deutsche'} Übersetzung von " + colored(f"{voc['Ger' if mode == 0 else 'Eng'][order[0]]} ", "blue"))
+            print(
+                f"\nWas ist die {'Englische' if mode == 0 else 'Deutsche'} Übersetzung von " + colored(f"{voc['Ger' if mode == 0 else 'Eng'][order[0]]} ", "cyan"), end='')
+            user_input = input()
 
             if user_input.lower() == "exit":
                 break
@@ -144,20 +140,35 @@ class Trainer:
                     print(colored("Hast du dich vielleicht nur vertippt?\n", "yellow"))
                 else:
                     print(colored("Leider falsch.", "red"))
-                    self.users_df.loc[order[0], username] = self.users_df.loc[order[0], username] + 1
+                    self.users_df.loc[order[0], self.username] = self.users_df.loc[order[0], self.username] + 1
                     random.shuffle(order)
 
         with open("users.csv", "w", newline="") as csvfile:
             self.users_df.to_csv(csvfile, sep=";", index=False)
 
-        if self.users_df[username].any():
+        if self.users_df[self.username].any():
             if input("Willst du die Vokabeln wissen, mit denen du Schwierigkeiten hast? (y/n) ") == "y":
                 hard = []
-                for i in range(len(voc_df)):
-                    if self.users_df.loc[i, username] > 0:
+                for i in range(len(self.voc_df)):
+                    if self.users_df.loc[i, self.username] > 0:
                         hard.append(i)
-                print("\n".join(f"{voc_df['Ger'][i]} - {voc_df['Eng'][i]}" for i in hard))
+                print("\n".join(colored(f"{self.voc_df['Ger'][i]}", "blue") + " - " + colored(f"{self.voc_df['Eng'][i]}", "magenta") for i in hard))
+        else:
+            print(colored("Super, du bist fertig und hast keine Fehler gemacht!!!", "green"))
 
-username = input("Was ist dein Nutzername? ")
+    def add_vokabulary(self, ger_v, eng_v):
 
-Trainer(username)
+
+        if input(f"\nType exit if there is something incorrect\n{ger_v} - {eng_v}\n") != "exit":
+            self.voc_df = self.voc_df.append({"Ger": ger_v, "Eng": eng_v}, ignore_index=True)
+            self.users_df = self.users_df.append({self.username: 0}, ignore_index=True)
+            with open("vokabeln.csv", "w", newline="") as f:
+                self.voc_df.to_csv(f, sep=";", index=False)
+            with open("users.csv", "w", newline="") as csvfile:
+                self.users_df.to_csv(csvfile, sep=";", index=False)
+        else:
+            with open("vokabeln.csv", "r") as f:
+                self.voc_df = pd.read_csv(f, sep=";", index=False)
+            print("nope")
+
+Trainer()
--- testing.py
+++ testing.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
 df = pd.read_csv("vokabeln.csv", sep=";")
-userdf = pd.read_csv("users.csv", sep=";")
+udf = pd.read_csv("users.csv", sep=";")
 
-print(userdf["Tim"].any())
+print(len(df))
+print((len(udf)))
+udf = pd.DataFrame({"Timo": [0 for i in range(len(df))]})
+udf.to_csv("users.csv", sep=";", index=False)
+print(len(udf))
+print(udf)
